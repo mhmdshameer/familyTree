9052e77e82f0a9fd0c5de89acc071f8a13d4c42c
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-04-09 21:06:23 +0530

    new update

---
+++ src/app/trpc/[trpc]/route.ts
@@ -0,0 +1,11 @@
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { appRouter } from '../../../trpc/routers/_app';
+
+export const GET = (req: Request) =>
+  fetchRequestHandler({
+    endpoint: '/trpc',
+    req,
+    router: appRouter,
+    createContext: () => ({}),
+  });
+export const POST = GET;
---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,4 @@
+import { router } from '../server';
+
+export const appRouter = router({});
+export type AppRouter = typeof appRouter;
